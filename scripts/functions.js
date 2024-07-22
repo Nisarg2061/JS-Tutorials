e006e7d0768c52cb09922dffc0ec10f129cba694
Nisarg2061 <khodkenisarg@gmail.com>
2024-07-22 17:38:02 +0530

    Update tutorial 5 files - Arrow functions, forEach

--- scripts/functions.js
+++ scripts/functions.js
@@ -12,8 +12,45 @@ function printName(nameStr) { // nameStr --> Function parameter
 
 printName("Nisarg"); // Passing with an argument
 
-// Function to calculate sum of two numbers
-function sum(a, b) {
+// Function to calculate counter of two numbers
+function counter(a, b) {
   return a + b;
 }
-console.log(sum(2,9));
+console.log(counter(2,9));
+
+// Arrow Function *** Modern day standard ***
+const arrowcounter = (a, b) => {
+  return a + b;
+}
+console.log(arrowcounter(1, 2));
+
+// Arrow function for multiplication
+const arrowMul = (a, b) => {
+  return a * b;
+}
+console.log(arrowMul(1, 88));
+
+// Arrow Function to find number of vowels in a string.
+const vowelsCounter = (word) => {
+  let count = 0; // The var that tracks the number of vowels
+  let vovels = ["a", "e", "i", "o", "u"]; // An array of vowles
+  for (const i of word) {
+    for (const j of vovels) {
+      if ( j == i) { // checks with each vowels in the array
+        count += 1; // Adds to the tracker if the match is found
+      }
+    }
+  }
+  return count; // returns the number of vowels in the string
+}
+
+let result = vowelsCounter("Nisarg");
+console.log("Number of vovels:", result); // Expected result : 2
+
+// forEach loop in Arrays using Arrow Functions
+const arr = [1, 2, 3, 4];
+
+arr.forEach((val) => {
+  console.log(val);
+});
+
