f8c6f0cfbb6aab79cece485cf33cf25051402725
Nisarg2061 <khodkenisarg@gmail.com>
2024-07-25 20:30:45 +0530

    Completed tutorial - 8

--- scripts/events.js
+++ scripts/events.js
@@ -1,13 +1,22 @@
-const btn1 = document.querySelector("#btn1");
-  let counter = 0;
-btn1.onclick = () => {
-  box.innerText = counter;
-  counter += 1;
+let curr = "Dark";
+
+const btn = document.querySelector("#modeBtn"); // selecting the button taht toggles mode
+const body = document.querySelector("body"); // selecting body
+body.classList.add(curr); // Default Class
+btn.innerText = "Mode: " + curr; // Default mode text
+
+const modes = (mode) => { // Function to change modes
+  body.classList.remove(curr); // removing the current class
+  body.classList.add(mode); // adding the class corresponding to the mode [Light <--> Dark]
+  btn.innerText = "Mode: " + mode; // Toggle button text
+  curr = mode;
+} 
+
+const changeMode = () => { // On clik trigger
+  if(curr === "Dark"){ //Checking the current mode
+    modes("Light"); // calling the mode changer
+  } else {
+    modes("Dark"); // Calling the mode changer
+  }
 }
- 
-let a = 0;
-const box = document.querySelector("#box");
-box.onmouseover = () => {
-  box.innerText = counter;
-  counter += 1;
-}
+btn.addEventListener("click",changeMode); // when the event is triggered
