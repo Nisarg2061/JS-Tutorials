b4623ebea24feb77f0cf4c74a80f0eb001dc4293
Nisarg2061 <khodkenisarg@gmail.com>
2024-07-25 20:49:35 +0530

    Completed tutorial - 8

--- scripts/events.js
+++ scripts/events.js
@@ -1,22 +1,23 @@
 let curr = "Dark";
-
+//-----------------------------------------------------------------------------------------//
 const btn = document.querySelector("#modeBtn"); // selecting the button taht toggles mode
 const body = document.querySelector("body"); // selecting body
 body.classList.add(curr); // Default Class
 btn.innerText = "Mode: " + curr; // Default mode text
-
+//-----------------------------------------------------------------------------------------//
 const modes = (mode) => { // Function to change modes
   body.classList.remove(curr); // removing the current class
   body.classList.add(mode); // adding the class corresponding to the mode [Light <--> Dark]
   btn.innerText = "Mode: " + mode; // Toggle button text
   curr = mode;
 } 
-
+//-----------------------------------------------------------------------------------------//
 const changeMode = () => { // On clik trigger
-  if(curr === "Dark"){ //Checking the current mode
+  if (curr === "Dark") { //Checking the current mode
     modes("Light"); // calling the mode changer
   } else {
     modes("Dark"); // Calling the mode changer
   }
 }
-btn.addEventListener("click",changeMode); // when the event is triggered
+btn.addEventListener("click", changeMode); // when the event is triggered
+//-----------------------------------------------------------------------------------------//
