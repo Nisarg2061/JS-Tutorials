433389779cad7955505278a2b9e63ddf5c9de381
Nisarg2061 <khodkenisarg@gmail.com>
2024-07-28 19:22:58 +0530

    Added tutorial 11 files - Classes and Objects

--- scripts/class.js
+++ scripts/class.js
@@ -1,7 +1,40 @@
-const student = {
-  fname : "Nisarg Khodke",
-  marks : 99,
-  grades : function () {
-    console.log("%s's marks are %d.\n", this.fname, this.marks);
-  },
-};
+var count = 0
+//-----------------------------------------------------------------------------------------//
+class car {
+  constructor(brand) {
+    count += 1;
+    this.brand = brand;
+    console.log("Object %d Created...", count);
+  }
+  
+  start() {
+    console.log("start");
+  }
+
+  stop() {
+    console.log("stop");
+  }
+}  
+//-----------------------------------------------------------------------------------------//
+let fortunuer = new car("Toyota");
+console.log(fortunuer);
+let creta = new car("Hyndai");
+console.log(creta);
+//-----------------------------------------------------------------------------------------//
+class person {
+  eat() {
+    console.log("eat");
+  }
+
+  sleep() {
+    console.log("sleep");
+  }  
+}
+
+class engineer extends person {
+  work() {
+    console.log("Solver of all problems");
+  }
+}
+
+let nisarg = new engineer;
